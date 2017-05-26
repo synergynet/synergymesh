8d30ed5afb6bc2b20b8812f4156b702ca8c710f9
jamcnaughton <james.andrew.mcnaughton@gmail.com>
2017-05-26 11:09:23 +0100

    CSS Simplification
    Made it so both an app's student and teacher versions can share the same stylesheet.

--- apps/protomysteries/src/protomysteries_student_app.js
+++ apps/protomysteries/src/protomysteries_student_app.js
@@ -3,7 +3,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
-define(["require", "exports", 'common/src/synergymesh_app'], function (require, exports, synergymesh_app_1) {
+define(["require", "exports", 'common/src/synergymesh_app', 'common/src/listeners/drag_listener', 'common/src/items/text_item', 'common/src/utils/transformations'], function (require, exports, synergymesh_app_1, drag_listener_1, text_item_1, transformations_1) {
     "use strict";
     var ProtomysteriesStudentApp = (function (_super) {
         __extends(ProtomysteriesStudentApp, _super);
@@ -11,6 +11,11 @@ define(["require", "exports", 'common/src/synergymesh_app'], function (require, 
             _super.call(this);
         }
         ProtomysteriesStudentApp.prototype.addContents = function () {
+            var textItem = new text_item_1.TextItem(this.svg, '<b>Hello World!</b> This is a really long string!', 100, 80, 'clue', 'clue-bg', 'clue-text');
+            transformations_1.Transformations.setTranslation(textItem.asItem(), this.vizWidth / 2, this.vizHeight / 2);
+            transformations_1.Transformations.setScale(textItem.asItem(), 2);
+            transformations_1.Transformations.setRotation(textItem.asItem(), 45);
+            new drag_listener_1.DragListener(textItem.asItem(), true);
         };
         return ProtomysteriesStudentApp;
     }(synergymesh_app_1.SynergyMeshApp));
--- apps/protomysteries/src/protomysteries_student_app.ts
+++ apps/protomysteries/src/protomysteries_student_app.ts
@@ -1,4 +1,7 @@
 import {SynergyMeshApp} from 'common/src/synergymesh_app';
+import {DragListener} from 'common/src/listeners/drag_listener';
+import {TextItem} from 'common/src/items/text_item';
+import {Transformations} from 'common/src/utils/transformations';
   
  /**
  * Protomysteries student app.
@@ -21,6 +24,14 @@ export class ProtomysteriesStudentApp extends SynergyMeshApp {
 	protected addContents() {
 		
 		// TODO Hard coded text clues and images + header.
+				
+		// Create text item.
+		let textItem =
+			new TextItem(this.svg, '<b>Hello World!</b> This is a really long string!', 100, 80, 'clue', 'clue-bg', 'clue-text');
+		Transformations.setTranslation(textItem.asItem(), this.vizWidth/2, this.vizHeight/2);
+		Transformations.setScale(textItem.asItem(), 2);
+		Transformations.setRotation(textItem.asItem(), 45);
+		new DragListener(textItem.asItem(), true);		
 				
 	}
 	
