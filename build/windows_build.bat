6b959d5c2b2703c6baf6cd9ee121352e684193ce
JAMcNaughton <james.andrew.mcnaughton@gmail.com>
2017-06-07 16:06:33 +0100

    Updated project name.
    Now all lowercase.

--- build/windows_build.bat
+++ build/windows_build.bat
@@ -2,24 +2,24 @@
 cd output
 
 echo Deleting old build.
-rmdir /s /q SynergyMesh
+rmdir /s /q synergymesh
 
-echo Creating new output/SynergyMesh folder.
-md SynergyMesh
+echo Creating new output/synergymesh folder.
+md synergymesh
 
 echo Copying in apps.
-xcopy /i "../../apps" "SynergyMesh/apps" /e
+xcopy /i "../../apps" "synergymesh/apps" /e
 
 echo Copying in common.
-xcopy /i "../../common" "SynergyMesh/common" /e
+xcopy /i "../../common" "synergymesh/common" /e
 
 echo Copying in lib.
-xcopy /i "../../lib" "SynergyMesh/lib" /e
+xcopy /i "../../lib" "synergymesh/lib" /e
 
 echo Copying in server.
-xcopy /i "../../server" "SynergyMesh/server" /e
+xcopy /i "../../server" "synergymesh/server" /e
 
-cd SynergyMesh
+cd synergymesh
 
 echo Deleting all .ts files.
 del /S *.ts
